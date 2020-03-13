204f10c6aae5e361e3a480f16e41a457b8f2a700
Tom Russell <tomalrussell@gmail.com>
2020-03-13 13:49:53 +0000

    Fix source running prep displacement.r
    The ellipsis character on line 29 pf prep displacement.r was causing the
    processing to fail. Running source() with forced UTF-8 encoding fixed it
    for me.

--- prep all data.r
+++ prep all data.r
@@ -3,7 +3,7 @@
 ##
 source("prep destitution.r")
 source("prep digital exclusion.r")
-source("prep displacement.r")
+source("prep displacement.r", encoding="utf-8")
 source("prep fires.r")
 source("prep flood risks.r")
 source("prep healthy life expectancy - CCG.r")
